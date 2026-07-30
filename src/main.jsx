80c8cef356a73babfd37cf4f469ff45eb6877429
salambasitdayo2019-debug <salambasitdayo2019@gmail.com>
2026-07-30 12:30:59 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './ArcTestnetDApp.jsx'
+import App, { ErrorBoundary } from './ArcTestnetDApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
